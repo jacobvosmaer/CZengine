43721fb272de370197865ba2436553d2c1c9f033
Jacob Vosmaer <contact@jacobvosmaer.nl>
2023-12-02 12:19:42 +0100

    Saw wave kind of working

--- CZengine.cpp
+++ CZengine.cpp
@@ -16,6 +16,15 @@ struct {
   float freq;
 } dco;
 
+struct {
+  float M;
+} dcw;
+
+float dcw_process(float phase) {
+  phase = (phase < dcw.M) ? phase / dcw.M : phase / (2.0 - dcw.M);
+  return cosf(pi * phase);
+}
+
 float hztofreq(float hz) { return hz / hw.AudioSampleRate(); }
 
 static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
@@ -23,11 +32,12 @@ static void AudioCallback(AudioHandle::InterleavingInputBuffer in,
                           size_t size) {
   hw.ProcessAllControls();
   dco.freq = hztofreq(20.0 * powf(2.0, 11.0 * hw.knob2.Process()));
+  dcw.M = 0.1 + 1.9 * hw.knob1.Process();
   for (int i = 0; i < (int)size; i += 2) {
     dco.phase += dco.freq;
     while (dco.phase > 2.0)
       dco.phase -= 2.0;
-    out[i] = out[i + 1] = sinf(pi * dco.phase);
+    out[i] = out[i + 1] = dcw_process(dco.phase);
   }
 }
 
